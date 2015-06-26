9ff55e799d1e5d3fe022431816bd6c61f3bb9fbc
Joey Freund <joeyfreund@gmail.com>
2015-06-26 19:13:16 -0400

    Adding website to go with the workshop.

--- setup-pacer.sh
+++
@@ -1,8 +0,0 @@
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
- \curl -sSL https://get.rvm.io | bash -s stable
-
-rvm install jruby
-gem install pacer
-
-echo -e "\n\nYou're good to go."
-echo -e "Next, run the \033[0;32m irb \033[0m and start using Pacer interactively."
