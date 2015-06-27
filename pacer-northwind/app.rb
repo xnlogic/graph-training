3956b63cc70c531f52a5b4a85b7f93062ec41abd
Joey Freund <joeyfreund@gmail.com>
2015-06-27 18:22:38 -0400

    Removing unnecessary helper functions (what do you know ... Pacer already has these utilities built in).

--- pacer-northwind/app.rb
+++ pacer-northwind/app.rb
@@ -2,7 +2,6 @@ module NorthWind
 
 	require 'pacer'
 
-
 	def self.load_extensions
 		Dir["pacer-ext/*.rb"].each {|extension| load extension}
 	end
@@ -13,36 +12,8 @@ module NorthWind
 		g
 	end
 
-	def self.schema_info(g, type)
-		r = nil
-		if type.nil?
-			r = g.v
-		else
-			r = g.v(type: type)
-		end
-
-		{v: _vertex_info(r), e: _edge_info(r)}
-	end
-
-	# Go over the vertices in the given vertex_route, and return
-	# a hash where:
-	# - The keys are all the different `type`s.
-	# - The Values are all property names of vertices with that type.
-	def self._vertex_info(vertex_route)
-		info = Hash.new { Set.new }
-		vertex_route.each  do |v| 
-			info[v[:type]] =  info[v[:type]].add(v.properties.keys)
-		end
-		info
-	end
-
-	def self._edge_info(vertex_route)
-		info = Set.new
-		vertex_route.both_e.each{ |e| info.add([e.out_v.first[:type], e.label, e.in_v.first[:type]]) }
-		info
-	end
-
-
+	# TIP: Given a graph g, you can get a full report of its structure (vertices and edges) using
+	# Pacer::Utils::GraphAnalysis.structure g
 
 	load_extensions()
 
